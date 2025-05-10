23d2597dda4d9b8b2d327327d51ad13044650b58
Valentin Gagarin <valentin@gagarin.work>
2025-05-10 02:42:24 +0200

    test simple templating

--- default.nix
+++ default.nix
@@ -48,4 +48,5 @@ in
         '';
       };
     };
+  test = import ./test.nix;
 }
--- test.nix
+++ test.nix
@@ -1,6 +1,50 @@
 let
   inherit (import ./. { }) lib;
-  eval = modules: lib.evalModules { inherit modules; };
+  eval = modules: (lib.evalModules { inherit modules; }).config;
+  base =
+    {
+      config,
+      options,
+      lib,
+      ...
+    }:
+    let
+      inherit (lib) mkOption types;
+    in
+    {
+      options.example = mkOption {
+        type =
+          with types;
+          submodule (example: {
+            options = {
+              data = mkOption {
+                type = with types; int;
+              };
+              __toString = mkOption {
+                type = with types; functionTo str;
+                default = _: toString example.config.data;
+              };
+              output = mkOption {
+                type = types.str;
+                default = toString example.config;
+                readOnly = true;
+              };
+            };
+          });
+      };
+    };
 in
 {
+  test-simple-template = {
+    expr =
+      (eval [
+        base
+        {
+          example = {
+            data = 1;
+          };
+        }
+      ]).example.output;
+    expected = "1";
+  };
 }

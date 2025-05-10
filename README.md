07db7b926b96e6635945831666c6b0ed68f09b0d
Valentin Gagarin <valentin@gagarin.work>
2025-05-10 04:42:31 +0200

    simplify and document

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Module system magic
+# Modular templates
 
-Dependent types bring fear.
+> Dependent types bring fear.
+
+This is an experiment to sketch a simpler, modular templating system for [htmnix](https://github.com/fricklerhandwerk/htmnix).
--- test.nix
+++ test.nix
@@ -1,114 +1,38 @@
 let
   inherit (import ./. { }) lib;
+
+  inherit (lib) mkOption types;
+
   eval = modules: (lib.evalModules { inherit modules; }).config;
-  base =
+
+  container =
     {
       config,
       options,
       lib,
       ...
     }:
-    let
-      inherit (lib) mkOption types;
-    in
     {
       options.example = mkOption {
+        description = "some arbitrary business logic data type";
         type =
           with types;
           submodule (example: {
             options = {
               data = mkOption {
-                type = with types; int;
-              };
-              __toString = mkOption {
-                type = with types; functionTo str;
-                default = self: toString self.data;
-              };
-              output = mkOption {
-                type = types.str;
-                default = toString example.config;
-                readOnly = true;
-              };
-            };
-          });
-      };
-    };
-  many-templates =
-    {
-      config,
-      options,
-      lib,
-      ...
-    }:
-    let
-      inherit (lib) mkOption types;
-    in
-    {
-      options.example = mkOption {
-        type =
-          with types;
-          submodule (example: {
-            options = {
-              data = mkOption {
+                description = "sample data";
                 type = with types; int;
               };
               templates = mkOption {
-                type = with types; attrsOf (functionTo str);
-              };
-              outputs = mkOption {
-                type = with types; attrsOf str;
-                default = lib.mapAttrs (name: template: template example.config) example.config.templates;
-                readOnly = true;
-              };
-            };
-          });
-      };
-      # NOTE: can't set it the type's `default` as that will always be overridden
-      config.example.templates.simple = value: toString value.data;
-    };
-  modular-templates =
-    {
-      config,
-      options,
-      lib,
-      ...
-    }:
-    let
-      inherit (lib) mkOption types;
-    in
-    {
-      options.example = mkOption {
-        type =
-          with types;
-          submodule (example: {
-            options = {
-              data = mkOption {
-                type = with types; int;
-              };
-              templates = mkOption {
+                description = "templates for rendering the business data to a string";
                 type =
                   with types;
                   attrsOf (submoduleWith {
-                    modules = [
-                      (template: {
-                        options.__toString = mkOption {
-                          type = with types; functionTo str;
-                        };
-                        options.__value = mkOption {
-                          description = ''
-                            all native values of the template, can be used for overriding it
-                          '';
-                          type = with types; attrs;
-                          default = lib.removeAttrs template.config [
-                            "__toString"
-                            "__value"
-                          ];
-                        };
-                      })
-                    ];
+                    modules = [ template-interface ];
                   });
               };
               outputs = mkOption {
+                description = "renderings based on configured templates";
                 type = with types; attrsOf str;
                 default = lib.mapAttrs (name: template: toString template) example.config.templates;
                 readOnly = true;
@@ -120,43 +44,56 @@ let
           });
       };
     };
+
+  template-interface = template: {
+    options.__toString = mkOption {
+      type = with types; functionTo str;
+    };
+    options.__value = mkOption {
+      description = ''
+        all native values of the template, can be used for overriding it
+      '';
+      type = with types; attrs;
+      default = lib.removeAttrs template.config [
+        "__toString"
+        "__value"
+      ];
+    };
+  };
+
+  fancy-template = fancy: {
+    options = {
+      custom-data = mkOption {
+        description = "template-specific parameters";
+        type = types.int;
+      };
+      nonsense = mkOption {
+        description = "more template-specific parameters";
+        type = types.str;
+        default = "";
+      };
+      __toString = mkOption {
+        description = "templates can in principle be used and rendered standalone";
+        type = with types; functionTo str;
+      };
+    };
+    config.__toString = _: toString fancy.config.custom-data + fancy.config.nonsense;
+  };
 in
 {
   test-modular-templates = {
     expr =
-      let
-        fancy =
-          fancy:
-          let
-            inherit (fancy.lib) mkOption types;
-          in
-          {
-            options = {
-              extra-data = mkOption {
-                type = types.int;
-              };
-              nonsense = mkOption {
-                type = types.str;
-                default = "";
-              };
-              __toString = mkOption {
-                type = with types; functionTo str;
-              };
-            };
-            config.__toString = _: toString fancy.config.extra-data + fancy.config.nonsense;
-          };
-      in
       (eval [
-        modular-templates
+        container
         {
           example = value: {
             data = 1;
             templates.fancy = {
-              imports = [ fancy ];
-              extra-data = value.config.data + 2;
+              imports = [ fancy-template ];
+              custom-data = value.config.data + 1;
             };
             templates.extra-fancy = extra-fancy: {
-              imports = [ fancy ];
+              imports = [ fancy-template ];
               # override an existing template!
               config = value.config.templates.fancy.__value // {
                 nonsense = "a";
@@ -167,49 +104,8 @@ in
       ]).example.outputs;
     expected = {
       simple = "1";
-      fancy = "3";
-      extra-fancy = "3a";
-    };
-  };
-  test-many-templates = {
-    expr =
-      (eval [
-        many-templates
-        {
-          example = {
-            data = 1;
-            templates.fancy = self: toString (self.data + 1);
-          };
-        }
-      ]).example.outputs;
-    expected = {
-      simple = "1";
       fancy = "2";
+      extra-fancy = "2a";
     };
-  };
-  test-override-template = {
-    expr =
-      (eval [
-        base
-        {
-          example = {
-            data = 1;
-            __toString = self: toString (self.data + 1);
-          };
-        }
-      ]).example.output;
-    expected = "2";
-  };
-  test-simple-template = {
-    expr =
-      (eval [
-        base
-        {
-          example = {
-            data = 1;
-          };
-        }
-      ]).example.output;
-    expected = "1";
   };
 }

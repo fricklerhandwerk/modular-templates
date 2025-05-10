7a257eb9b0bf5f2eb2b01cb8460a921275652e72
Valentin Gagarin <valentin@gagarin.work>
2025-05-10 03:05:34 +0200

    test using multiple templates

--- test.nix
+++ test.nix
@@ -33,8 +33,57 @@ let
           });
       };
     };
+  many-templates =
+    {
+      config,
+      options,
+      lib,
+      ...
+    }:
+    let
+      inherit (lib) mkOption types;
+    in
+    {
+      options.example = mkOption {
+        type =
+          with types;
+          submodule (example: {
+            options = {
+              data = mkOption {
+                type = with types; int;
+              };
+              templates = mkOption {
+                type = with types; attrsOf (functionTo str);
+              };
+              outputs = mkOption {
+                type = with types; attrsOf str;
+                default = lib.mapAttrs (name: template: template example.config) example.config.templates;
+                readOnly = true;
+              };
+            };
+          });
+      };
+      # NOTE: can't set it the type's `default` as that will always be overridden
+      config.example.templates.simple = value: toString value.data;
+    };
 in
 {
+  test-many-templates = {
+    expr =
+      (eval [
+        many-templates
+        {
+          example = {
+            data = 1;
+            templates.fancy = self: toString (self.data + 1);
+          };
+        }
+      ]).example.outputs;
+    expected = {
+      simple = "1";
+      fancy = "2";
+    };
+  };
   test-override-template = {
     expr =
       (eval [
